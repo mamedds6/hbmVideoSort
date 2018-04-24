1ccb45e11498edae701b9db70f0fb892bea7961b
mamedds6 <darekmichalski@onet.eu>
2018-04-24 13:14:02 +0100

    Add project files.

---
+++ hbm1/Form1.Designer.cs
@@ -0,0 +1,439 @@
+﻿namespace hbm1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.labelFolder = new System.Windows.Forms.Label();
+            this.textBoxFolder = new System.Windows.Forms.TextBox();
+            this.buttonJedziemy = new System.Windows.Forms.Button();
+            this.labelRozszerzenie = new System.Windows.Forms.Label();
+            this.textBoxRozszerzenie = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelAuto = new System.Windows.Forms.Label();
+            this.textBoxAuto = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.labelPrzejazdy = new System.Windows.Forms.Label();
+            this.flp = new System.Windows.Forms.FlowLayoutPanel();
+            this.A = new System.Windows.Forms.TextBox();
+            this.B = new System.Windows.Forms.TextBox();
+            this.C = new System.Windows.Forms.TextBox();
+            this.D = new System.Windows.Forms.TextBox();
+            this.E = new System.Windows.Forms.TextBox();
+            this.F = new System.Windows.Forms.TextBox();
+            this.G = new System.Windows.Forms.TextBox();
+            this.H = new System.Windows.Forms.TextBox();
+            this.I = new System.Windows.Forms.TextBox();
+            this.J = new System.Windows.Forms.TextBox();
+            this.K = new System.Windows.Forms.TextBox();
+            this.L = new System.Windows.Forms.TextBox();
+            this.M = new System.Windows.Forms.TextBox();
+            this.N = new System.Windows.Forms.TextBox();
+            this.O = new System.Windows.Forms.TextBox();
+            this.P = new System.Windows.Forms.TextBox();
+            this.Q = new System.Windows.Forms.TextBox();
+            this.R = new System.Windows.Forms.TextBox();
+            this.S = new System.Windows.Forms.TextBox();
+            this.T = new System.Windows.Forms.TextBox();
+            this.U = new System.Windows.Forms.TextBox();
+            this.V = new System.Windows.Forms.TextBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.flp.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelFolder
+            // 
+            this.labelFolder.AutoSize = true;
+            this.labelFolder.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelFolder.Location = new System.Drawing.Point(187, 9);
+            this.labelFolder.Name = "labelFolder";
+            this.labelFolder.Size = new System.Drawing.Size(38, 15);
+            this.labelFolder.TabIndex = 5;
+            this.labelFolder.Text = "Folder";
+            this.labelFolder.Click += new System.EventHandler(this.labelFolder_Click);
+            // 
+            // textBoxFolder
+            // 
+            this.textBoxFolder.Location = new System.Drawing.Point(231, 6);
+            this.textBoxFolder.Name = "textBoxFolder";
+            this.textBoxFolder.ReadOnly = true;
+            this.textBoxFolder.Size = new System.Drawing.Size(398, 20);
+            this.textBoxFolder.TabIndex = 6;
+            this.textBoxFolder.Click += new System.EventHandler(this.labelFolder_Click);
+            // 
+            // buttonJedziemy
+            // 
+            this.buttonJedziemy.Location = new System.Drawing.Point(12, 177);
+            this.buttonJedziemy.Name = "buttonJedziemy";
+            this.buttonJedziemy.Size = new System.Drawing.Size(101, 39);
+            this.buttonJedziemy.TabIndex = 7;
+            this.buttonJedziemy.Text = "Jedziemy!";
+            this.buttonJedziemy.UseVisualStyleBackColor = true;
+            this.buttonJedziemy.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelRozszerzenie
+            // 
+            this.labelRozszerzenie.AutoSize = true;
+            this.labelRozszerzenie.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelRozszerzenie.Location = new System.Drawing.Point(12, 9);
+            this.labelRozszerzenie.Name = "labelRozszerzenie";
+            this.labelRozszerzenie.Size = new System.Drawing.Size(72, 15);
+            this.labelRozszerzenie.TabIndex = 8;
+            this.labelRozszerzenie.Text = "Rozszerzenie";
+            // 
+            // textBoxRozszerzenie
+            // 
+            this.textBoxRozszerzenie.Location = new System.Drawing.Point(90, 6);
+            this.textBoxRozszerzenie.Name = "textBoxRozszerzenie";
+            this.textBoxRozszerzenie.Size = new System.Drawing.Size(69, 20);
+            this.textBoxRozszerzenie.TabIndex = 9;
+            this.textBoxRozszerzenie.Text = "mts";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelAuto);
+            this.panel1.Controls.Add(this.textBoxAuto);
+            this.panel1.Location = new System.Drawing.Point(12, 48);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(66, 87);
+            this.panel1.TabIndex = 12;
+            // 
+            // labelAuto
+            // 
+            this.labelAuto.AutoSize = true;
+            this.labelAuto.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelAuto.Location = new System.Drawing.Point(13, 19);
+            this.labelAuto.Name = "labelAuto";
+            this.labelAuto.Size = new System.Drawing.Size(31, 15);
+            this.labelAuto.TabIndex = 1;
+            this.labelAuto.Text = "Auto";
+            // 
+            // textBoxAuto
+            // 
+            this.textBoxAuto.Location = new System.Drawing.Point(3, 44);
+            this.textBoxAuto.Name = "textBoxAuto";
+            this.textBoxAuto.Size = new System.Drawing.Size(51, 20);
+            this.textBoxAuto.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(84, 70);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(597, 16);
+            this.flowLayoutPanel1.TabIndex = 11;
+            this.flowLayoutPanel1.WrapContents = false;
+            // 
+            // labelPrzejazdy
+            // 
+            this.labelPrzejazdy.AutoSize = true;
+            this.labelPrzejazdy.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelPrzejazdy.Location = new System.Drawing.Point(315, 48);
+            this.labelPrzejazdy.Name = "labelPrzejazdy";
+            this.labelPrzejazdy.Size = new System.Drawing.Size(54, 15);
+            this.labelPrzejazdy.TabIndex = 10;
+            this.labelPrzejazdy.Text = "Przejazdy";
+            // 
+            // flp
+            // 
+            this.flp.AutoScroll = true;
+            this.flp.Controls.Add(this.A);
+            this.flp.Controls.Add(this.B);
+            this.flp.Controls.Add(this.C);
+            this.flp.Controls.Add(this.D);
+            this.flp.Controls.Add(this.E);
+            this.flp.Controls.Add(this.F);
+            this.flp.Controls.Add(this.G);
+            this.flp.Controls.Add(this.H);
+            this.flp.Controls.Add(this.I);
+            this.flp.Controls.Add(this.J);
+            this.flp.Controls.Add(this.K);
+            this.flp.Controls.Add(this.L);
+            this.flp.Controls.Add(this.M);
+            this.flp.Controls.Add(this.N);
+            this.flp.Controls.Add(this.O);
+            this.flp.Controls.Add(this.P);
+            this.flp.Controls.Add(this.Q);
+            this.flp.Controls.Add(this.R);
+            this.flp.Controls.Add(this.S);
+            this.flp.Controls.Add(this.T);
+            this.flp.Controls.Add(this.U);
+            this.flp.Controls.Add(this.V);
+            this.flp.Location = new System.Drawing.Point(84, 89);
+            this.flp.Name = "flp";
+            this.flp.Size = new System.Drawing.Size(597, 59);
+            this.flp.TabIndex = 13;
+            this.flp.WrapContents = false;
+            this.flp.Paint += new System.Windows.Forms.PaintEventHandler(this.flp_Paint);
+            // 
+            // A
+            // 
+            this.A.Location = new System.Drawing.Point(3, 3);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(40, 20);
+            this.A.TabIndex = 1;
+            // 
+            // B
+            // 
+            this.B.Location = new System.Drawing.Point(49, 3);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(40, 20);
+            this.B.TabIndex = 2;
+            // 
+            // C
+            // 
+            this.C.Location = new System.Drawing.Point(95, 3);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(40, 20);
+            this.C.TabIndex = 3;
+            // 
+            // D
+            // 
+            this.D.Location = new System.Drawing.Point(141, 3);
+            this.D.Name = "D";
+            this.D.Size = new System.Drawing.Size(40, 20);
+            this.D.TabIndex = 4;
+            // 
+            // E
+            // 
+            this.E.Location = new System.Drawing.Point(187, 3);
+            this.E.Name = "E";
+            this.E.Size = new System.Drawing.Size(40, 20);
+            this.E.TabIndex = 5;
+            // 
+            // F
+            // 
+            this.F.Location = new System.Drawing.Point(233, 3);
+            this.F.Name = "F";
+            this.F.Size = new System.Drawing.Size(40, 20);
+            this.F.TabIndex = 6;
+            // 
+            // G
+            // 
+            this.G.Location = new System.Drawing.Point(279, 3);
+            this.G.Name = "G";
+            this.G.Size = new System.Drawing.Size(40, 20);
+            this.G.TabIndex = 7;
+            // 
+            // H
+            // 
+            this.H.Location = new System.Drawing.Point(325, 3);
+            this.H.Name = "H";
+            this.H.Size = new System.Drawing.Size(40, 20);
+            this.H.TabIndex = 8;
+            // 
+            // I
+            // 
+            this.I.Location = new System.Drawing.Point(371, 3);
+            this.I.Name = "I";
+            this.I.Size = new System.Drawing.Size(40, 20);
+            this.I.TabIndex = 9;
+            // 
+            // J
+            // 
+            this.J.Location = new System.Drawing.Point(417, 3);
+            this.J.Name = "J";
+            this.J.Size = new System.Drawing.Size(40, 20);
+            this.J.TabIndex = 10;
+            // 
+            // K
+            // 
+            this.K.Location = new System.Drawing.Point(463, 3);
+            this.K.Name = "K";
+            this.K.Size = new System.Drawing.Size(40, 20);
+            this.K.TabIndex = 11;
+            // 
+            // L
+            // 
+            this.L.Location = new System.Drawing.Point(509, 3);
+            this.L.Name = "L";
+            this.L.Size = new System.Drawing.Size(40, 20);
+            this.L.TabIndex = 12;
+            // 
+            // M
+            // 
+            this.M.Location = new System.Drawing.Point(555, 3);
+            this.M.Name = "M";
+            this.M.Size = new System.Drawing.Size(40, 20);
+            this.M.TabIndex = 13;
+            // 
+            // N
+            // 
+            this.N.Location = new System.Drawing.Point(601, 3);
+            this.N.Name = "N";
+            this.N.Size = new System.Drawing.Size(40, 20);
+            this.N.TabIndex = 14;
+            // 
+            // O
+            // 
+            this.O.Location = new System.Drawing.Point(647, 3);
+            this.O.Name = "O";
+            this.O.Size = new System.Drawing.Size(40, 20);
+            this.O.TabIndex = 15;
+            // 
+            // P
+            // 
+            this.P.Location = new System.Drawing.Point(693, 3);
+            this.P.Name = "P";
+            this.P.Size = new System.Drawing.Size(40, 20);
+            this.P.TabIndex = 16;
+            // 
+            // Q
+            // 
+            this.Q.Location = new System.Drawing.Point(739, 3);
+            this.Q.Name = "Q";
+            this.Q.Size = new System.Drawing.Size(40, 20);
+            this.Q.TabIndex = 17;
+            // 
+            // R
+            // 
+            this.R.Location = new System.Drawing.Point(785, 3);
+            this.R.Name = "R";
+            this.R.Size = new System.Drawing.Size(40, 20);
+            this.R.TabIndex = 18;
+            // 
+            // S
+            // 
+            this.S.Location = new System.Drawing.Point(831, 3);
+            this.S.Name = "S";
+            this.S.Size = new System.Drawing.Size(40, 20);
+            this.S.TabIndex = 19;
+            // 
+            // T
+            // 
+            this.T.Location = new System.Drawing.Point(877, 3);
+            this.T.Name = "T";
+            this.T.Size = new System.Drawing.Size(40, 20);
+            this.T.TabIndex = 20;
+            // 
+            // U
+            // 
+            this.U.Location = new System.Drawing.Point(923, 3);
+            this.U.Name = "U";
+            this.U.Size = new System.Drawing.Size(40, 20);
+            this.U.TabIndex = 21;
+            // 
+            // V
+            // 
+            this.V.Location = new System.Drawing.Point(969, 3);
+            this.V.Name = "V";
+            this.V.Size = new System.Drawing.Size(40, 20);
+            this.V.TabIndex = 22;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(124, 177);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(101, 39);
+            this.buttonClear.TabIndex = 14;
+            this.buttonClear.Text = "Wyczyść";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(558, 199);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(123, 17);
+            this.checkBox1.TabIndex = 15;
+            this.checkBox1.Text = "Zawsze na wierzchu";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 228);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.labelPrzejazdy);
+            this.Controls.Add(this.flp);
+            this.Controls.Add(this.textBoxRozszerzenie);
+            this.Controls.Add(this.labelRozszerzenie);
+            this.Controls.Add(this.buttonJedziemy);
+            this.Controls.Add(this.textBoxFolder);
+            this.Controls.Add(this.labelFolder);
+            this.Name = "Form1";
+            this.Text = "HBM sortowanie filmów";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.flp.ResumeLayout(false);
+            this.flp.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label labelFolder;
+        private System.Windows.Forms.TextBox textBoxFolder;
+        private System.Windows.Forms.Button buttonJedziemy;
+        private System.Windows.Forms.Label labelRozszerzenie;
+        private System.Windows.Forms.TextBox textBoxRozszerzenie;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelAuto;
+        private System.Windows.Forms.TextBox textBoxAuto;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label labelPrzejazdy;
+        private System.Windows.Forms.FlowLayoutPanel flp;
+        private System.Windows.Forms.TextBox A;
+        private System.Windows.Forms.TextBox B;
+        private System.Windows.Forms.TextBox C;
+        private System.Windows.Forms.TextBox D;
+        private System.Windows.Forms.TextBox E;
+        private System.Windows.Forms.TextBox F;
+        private System.Windows.Forms.TextBox G;
+        private System.Windows.Forms.TextBox H;
+        private System.Windows.Forms.TextBox I;
+        private System.Windows.Forms.TextBox J;
+        private System.Windows.Forms.TextBox K;
+        private System.Windows.Forms.TextBox L;
+        private System.Windows.Forms.TextBox M;
+        private System.Windows.Forms.TextBox N;
+        private System.Windows.Forms.TextBox O;
+        private System.Windows.Forms.TextBox P;
+        private System.Windows.Forms.TextBox Q;
+        private System.Windows.Forms.TextBox R;
+        private System.Windows.Forms.TextBox S;
+        private System.Windows.Forms.TextBox T;
+        private System.Windows.Forms.TextBox U;
+        private System.Windows.Forms.TextBox V;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ hbm1/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace hbm1
+{
+    public partial class Form1 : Form
+    {
+        private void nazwijLabele()
+        {
+            foreach (TextBox tb in flp.Controls)
+            {
+                tb.Size = new Size(40, 20);
+                Label label = new Label();
+                label.Name = "label" + tb.Name;
+                label.Text = tb.Name;
+                label.MaximumSize = new Size(20, 13);
+                Padding dupa = new Padding(15, 1, 11, 1);
+                label.Margin = dupa;
+                flowLayoutPanel1.Controls.Add(label);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            nazwijLabele();
+        }
+
+        private void labelFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFolder.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void utworzSciezke()
+        {
+            if (textBoxAuto.Text != "" && textBoxFolder.Text != "")
+            {
+                string sciezka = textBoxFolder.Text + "\\" + textBoxAuto.Text;
+                System.IO.Directory.CreateDirectory(sciezka);
+                buttonJedziemy.BackColor = Color.LightGray;
+            }
+            else
+            {
+                //textBoxFolder.BackColor = Color.Red;
+                //textBoxAuto.BackColor = Color.Red;
+                buttonJedziemy.BackColor = Color.Red;
+            }
+        }
+
+        private void przeniesPliki()
+        {
+            foreach (TextBox tb in flp.Controls)
+            {
+                if (tb.Text != "" && int.TryParse(tb.Text, out int n))
+                {
+                    tb.BackColor = Color.White;
+                    string plik = "\\" + tb.Name + " (" + tb.Text + ")." + textBoxRozszerzenie.Text;
+                    string org = textBoxFolder.Text + plik;
+                    string kopia = textBoxFolder.Text + "\\" + textBoxAuto.Text + plik;
+                    if (File.Exists(org))
+                        File.Move(org, kopia);
+                        //File.Copy(org, kopia, true);                
+                    else
+                        tb.BackColor = Color.OrangeRed;
+                }
+                else
+                    tb.BackColor = Color.LightGray;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //sprawdza folder i auto i...
+            utworzSciezke();
+
+            //sprawdza czy nie puste i liczba w przejezdzie
+            //i czy plik istnieje i...
+            przeniesPliki();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            flp.Size = new Size(ActiveForm.Size.Width - 100, flp.Height);
+            flowLayoutPanel1.Size = new Size(ActiveForm.Size.Width - 100, flowLayoutPanel1.Height);
+            
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxAuto.Text = "";
+            buttonJedziemy.BackColor = Color.LightGray;
+            foreach (TextBox tb in flp.Controls)
+            {
+                tb.BackColor = Color.White;
+                tb.Text = "";
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) ActiveForm.TopMost = true;
+            else ActiveForm.TopMost = false;
+        }
+
+        private void flp_Paint(object sender, PaintEventArgs e)
+        {
+            flowLayoutPanel1.HorizontalScroll.Value = flp.HorizontalScroll.Value;
+        }
+    }
+}
